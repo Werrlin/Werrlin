bb1072f8305de86ef41a03359020aa3d897be413
werrlin <lin1024p@gmail.com>
2025-03-20 21:30:20 +0800

    .

---
+++ tools/README.md
@@ -0,0 +1,37 @@
+1、基础模块
+
+```markdown
+Types.h			一些重定义操作
+时间类 + 定时器任务
+异常处理
+配置数据读取模块
+日志模块
+线程标识符
+阻塞队列的实现
+任务队列
+message模块
+messageQueue
+线程池
+连接池
+内存池
+Buffer 缓冲区
+Channel和Poller
+自动序列化反序列化CPP代码生成模块
+
+```
+
+
+
+
+brpc  
+concurrentqueue
+grpc
+inih
+librdkafka
+liburing
+nginx
+protobuf
+readerwriterqueue
+redis-plus-plus
+spdlog
+workflow
---
+++ tools/common/interface/TypeId.h
@@ -0,0 +1,28 @@
+// TypeId.h
+/**
+ * @brief 基础模块提供了类型的重命名.
+ * 
+ * 包含C++常用类型的重命名。
+ */
+
+
+ #ifndef    __TYPEDEID_H
+ #define    __TYPEDEID_H
+ 
+ 
+typedef         char                        ods_char8;
+typedef         unsigned char               ods_byte8;
+typedef         short                       ods_shour16;
+typedef         unsigned short              ods_ushour16;
+typedef         int                         ods_int32;
+typedef         unsigned int                ods_uint32;
+typedef         long                        ods_long32;
+typedef         unsigned long               ods_ulong32;
+typedef         double                      ods_double64;
+typedef         float                       ods_float32;
+typedef         bool                        ods_bool;
+typedef         long long int               ods_int64;
+typedef         unsigned long long int      ods_uint64;
+
+
+ #endif     //  #ifndef    __TYPEDEID_H
